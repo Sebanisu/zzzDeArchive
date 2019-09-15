b3a71141784e367ba71be1bc3942930749769b88
Robert Russell <peeceseven@gmail.com>
2019-09-15 01:55:17 -0400

    updated version numbers.

--- zzz/Properties/AssemblyInfo.cs
+++ zzz/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("zzz")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("zzz.dll for front ends to use to unarchive and archive zzz files for ff8 remaster")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("zzz")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.6.5")]
+[assembly: AssemblyFileVersion("0.1.6.5")]
