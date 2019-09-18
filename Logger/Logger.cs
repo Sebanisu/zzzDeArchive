10f20d0f9d78d683269f1e1705d45290d594d4e8
Robert Russell <peeceseven@gmail.com>
2019-09-17 20:46:34 -0400

    Logger to log what was put on the screen.
    so the people having issues had something to send us.

---
+++ Logger/Logger.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+namespace _Logger
+{
+    public class Logger : IDisposable
+    {
+        private static Logger self = new Logger();
+        private FileStream fs;
+        private StreamWriter sw;
+
+        public Logger()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "log.txt");
+            fs = File.Open(path, FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
+            sw = new StreamWriter(fs);
+        }
+
+        public static string Write(string @in, bool skipConsole = false, bool skipLog = false)
+        {
+            if (!skipConsole)
+                Console.Write(@in);
+            if (!skipLog)
+                self.sw.Write(@in);
+            return @in;
+        }
+
+        public static string WriteLine(string @in="", bool skipConsole = false, bool skipLog = false)
+        {
+            if (!skipConsole)
+                Console.WriteLine(@in);
+            if (!skipLog)
+                self.sw.WriteLine(@in);
+            return @in;
+        }
+
+        ~Logger()
+        {
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            if (sw != null && fs != null)
+            {
+                sw.Close();
+                sw = null;
+                fs = null;
+            }
+        }
+    }
+}
