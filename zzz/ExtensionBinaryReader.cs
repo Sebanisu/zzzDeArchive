fec29f84f7feb813db22c3376cd71d9f55284b0d
Robert Russell <peeceseven@gmail.com>
2019-10-07 10:27:45 -0400

    Added a stream overload.
    amend previous comment I forgot to save.
    
    Added a stream overload.

--- zzz/ExtensionBinaryReader.cs
+++ zzz/ExtensionBinaryReader.cs
@@ -24,7 +24,32 @@ namespace ZzzArchive
             }
         }
 
+        public static byte[] ReadAllBytes(this Stream reader)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                byte[] buffer = new byte[bufferSize];
+                int count;
+                while ((count = reader.Read(buffer, 0, buffer.Length)) != 0)
+                    ms.Write(buffer, 0, count);
+                return ms.ToArray();
+            }
+        }
+
         public static long WriteAllBytes(this BinaryWriter writer, BinaryReader reader)
+        {
+            byte[] buffer = new byte[bufferSize];
+            int count;
+            long total = 0;
+            while ((count = reader.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                total += count;
+                writer.Write(buffer, 0, count);
+            }
+            return total;
+        }
+
+        public static long WriteAllBytes(this Stream writer, Stream reader)
         {
             byte[] buffer = new byte[bufferSize];
             int count;
